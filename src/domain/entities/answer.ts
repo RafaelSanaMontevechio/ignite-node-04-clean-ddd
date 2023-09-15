a958739d33ad3bfb2df35e6e22221975c26dd75a
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-09-15 09:02:51 -0300

    feat: abstracting entity creation

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,12 +1,31 @@
 import { Entity } from '../../core/entities/entity';
+import { Optional } from '../../core/types/optional';
+import { UniqueEntityID } from '../../core/entities/unique-entity-id';
 
 interface AnswerProps {
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
   content: string;
-  authorId: string;
-  questionId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content;
+  }
+
+  static create(
+    props: Optional<AnswerProps, 'createdAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const answer = new Answer(
+      {
+        ...props,
+        createdAt: new Date(),
+      },
+      id,
+    );
+
+    return answer;
   }
 }
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,5 +1,6 @@
 import { AnswersRepository } from './../repositories/answers-repository';
 import { Answer } from './../entities/answer';
+import { UniqueEntityID } from '../../core/entities/unique-entity-id';
 interface AnswerQuestionUseCaseRequest {
   instructorId: string;
   questionId: string;
@@ -14,10 +15,10 @@ export class AnswerQuestionUseCase {
     questionId,
     content,
   }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      authorId: instructorId,
-      questionId,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
     });
 
     await this.answersRepository.create(answer);
