1148aff82086de07f4859613a7dd8531b13c1b7b
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-09-19 08:55:47 -0300

    feat: add factory to create a question for tests

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,72 +1,72 @@
-import dayjs from 'dayjs'
+import dayjs from 'dayjs';
 
-import { Entity } from '@/core/entities/entity'
-import { Optional } from '@/core/types/optional'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Entity } from '@/core/entities/entity';
+import { Optional } from '@/core/types/optional';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
-import { Slug } from './value-objects/slug'
+import { Slug } from './value-objects/slug';
 
-interface QuestionProps {
-  authorId: UniqueEntityID
-  bestAnswerId?: UniqueEntityID
-  title: string
-  content: string
-  slug: Slug
-  createdAt: Date
-  updatedAt?: Date
+export interface QuestionProps {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
+  title: string;
+  content: string;
+  slug: Slug;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 export class Question extends Entity<QuestionProps> {
   get authorId() {
-    return this.props.authorId
+    return this.props.authorId;
   }
 
   get bestAnswerId() {
-    return this.props.bestAnswerId
+    return this.props.bestAnswerId;
   }
 
   get title() {
-    return this.props.title
+    return this.props.title;
   }
 
   set title(title: string) {
-    this.props.title = title
-    this.props.slug = Slug.createFromText(title)
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
 
-    this.touch()
+    this.touch();
   }
 
   get content() {
-    return this.props.content
+    return this.props.content;
   }
 
   set content(content: string) {
-    this.props.content = content
+    this.props.content = content;
 
-    this.touch()
+    this.touch();
   }
 
   get slug() {
-    return this.props.slug
+    return this.props.slug;
   }
 
   get createdAt() {
-    return this.props.createdAt
+    return this.props.createdAt;
   }
 
   get updatedAt() {
-    return this.props.updatedAt
+    return this.props.updatedAt;
   }
 
   get isNew(): boolean {
-    return dayjs().diff(this.createdAt, 'days') <= 3
+    return dayjs().diff(this.createdAt, 'days') <= 3;
   }
 
   get excerpt() {
-    return this.content.substring(0, 120).trimEnd().concat('...')
+    return this.content.substring(0, 120).trimEnd().concat('...');
   }
 
   private touch() {
-    this.props.updatedAt = new Date()
+    this.props.updatedAt = new Date();
   }
 
   static create(
@@ -80,8 +80,8 @@ export class Question extends Entity<QuestionProps> {
         createdAt: new Date(),
       },
       id,
-    )
+    );
 
-    return question
+    return question;
   }
 }
