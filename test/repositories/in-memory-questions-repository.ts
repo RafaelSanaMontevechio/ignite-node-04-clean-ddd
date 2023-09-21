d89648562a9cc136a3faf56357a94aa5313c90cd
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-09-21 19:21:49 -0300

    feat: add edit question use case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -14,7 +14,7 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     return question;
   }
 
-  async findBySlug(slug: string): Promise<Question | null> {
+  async findBySlug(slug: string) {
     const question = this.items.find((item) => item.slug.value === slug);
 
     if (!question) {
@@ -22,6 +22,12 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     }
 
     return question;
+  }
+
+  async save(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+    this.items[itemIndex] = question;
   }
 
   async create(question: Question) {
