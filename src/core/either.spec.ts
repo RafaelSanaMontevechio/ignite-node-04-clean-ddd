89c6bd3a9f6584da15b925102dbb6d359b0d2b66
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-09-26 09:22:30 -0300

    feat: add generic errors

--- src/core/either.spec.ts
+++ src/core/either.spec.ts
@@ -15,13 +15,13 @@ test('Success result', () => {
   //     console.log(result.value);
   //   }
 
-  expect(result.isRight).toBe(true);
-  expect(result.isLeft).toBe(false);
+  expect(result.isRight()).toBe(true);
+  expect(result.isLeft()).toBe(false);
 });
 
 test('Error result', () => {
   const result = doSomething(false);
 
-  expect(result.isLeft).toBe(true);
-  expect(result.isRight).toBe(false);
+  expect(result.isLeft()).toBe(true);
+  expect(result.isRight()).toBe(false);
 });
