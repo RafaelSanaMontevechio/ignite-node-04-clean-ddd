a179e6926c7514c6102842c078180a0ca3074045
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-09-29 09:08:50 -0300

    refactor: refactor edit question test

---
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -0,0 +1,16 @@
+import { QuestionsAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository';
+import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment';
+
+export class InMemoryQuestionAttachmentsRepository
+  implements QuestionsAttachmentsRepository
+{
+  public items: QuestionAttachment[] = [];
+
+  async findManyByQuestionId(questionId: string) {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() === questionId,
+    );
+
+    return questionAttachments;
+  }
+}
