bb648319fa93fb4757e2c89aa40f73bc6725dac7
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2023-09-30 16:34:12 -0300

    feat: add question best answer chosen event

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -6,6 +6,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 import { Slug } from './value-objects/slug';
 import { QuestionAttachmentList } from './question-attachment-list';
+import { QuestionBestAnswerChosenEvent } from '../events/question-best-answer-chosen-event';
 
 export interface QuestionProps {
   authorId: UniqueEntityID;
@@ -27,6 +28,12 @@ export class Question extends AggregateRoot<QuestionProps> {
   }
 
   set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {
+      this.addDomainEvent(
+        new QuestionBestAnswerChosenEvent(this, bestAnswerId),
+      );
+    }
+
     this.props.bestAnswerId = bestAnswerId;
 
     this.touch();
